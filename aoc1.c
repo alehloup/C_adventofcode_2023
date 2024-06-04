c39f809595588bb3a282cca1767f0defa888a679
Alessandro <alessandroferreira@cremesp.org.br>
2024-06-03 21:42:56 -0300

    first aoc

--- aoc1.c
+++ aoc1.c
@@ -1,1 +1,62 @@
-advent of code 1
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+static inline char number(char line[256], size_t idx, size_t len) {
+    static const char *const spelled[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    static size_t spelled_len[9] = {3, 3, 5, 4, 4, 3, 5, 5, 4};
+    size_t remaining = len - idx;
+
+    if (line[idx] >= '0' && line[idx] <= '9') {
+        return line[idx] - '0';
+    }
+    for (unsigned int i = 0; i < 9; ++i) {
+        if (remaining >= spelled_len[i] && memcmp(&line[idx], spelled[i], spelled_len[i])==0) {
+            return (char)i+1;
+        }
+    }
+
+    (void)len;
+    return 0;
+}
+
+static inline void run(void) {
+    size_t sum = 0;
+    char line[256];
+    FILE *f = fopen("aoc1.txt", "r");
+        while (fgets(line, 256, f) != 0) {
+            char first = 0, last = 0;
+            size_t len = strlen(line);
+            
+            size_t i;
+            for (i = 0; i < len; ++i) {
+                char n = number(line, i, len);
+                if (n != 0) {
+                    first = n;
+                    break;
+                }
+            }
+            for (i = 1; i <= len; ++i) {
+                size_t idx = len - i;
+                char n = number(line, idx, len);
+                if (n != 0) {
+                    last = n;
+                    break;
+                }
+            }
+
+            sum += (size_t)first*10 + (size_t)last;
+
+            //printf("%d%d %s", first, last, line);
+        }
+        printf("sum: %zu\n", sum);
+    fclose(f);
+}
+
+int main(void) {
+    clock_t start = clock();
+    run();
+    printf("runned in %f seconds", (double)(clock() - start) / (double)CLOCKS_PER_SEC);
+
+    return 0;
+}
