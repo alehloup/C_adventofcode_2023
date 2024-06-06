57a9b471fdb27ae764aedab034ed9141143074dd
Alessandro <alessandroferreira@cremesp.org.br>
2024-06-05 22:04:37 -0300

    playing with parsing

--- aoc2.c
+++ aoc2.c
@@ -1,19 +1,50 @@
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
 static void run(void) {
-    char line[256];
+    int game;
+    size_t sum = 0;
+
     FILE *f = fopen("aoc2.txt", "r");
-    while (fgets(line, 256, f) != 0) {
-        printf("%s", line);
+    while (fscanf(f, " Game %d: ", &game) == 1) {
+        char color[8] = "";
+        unsigned int qty = 0;
+        char end = ',';
+
+        printf("\nGame %d: ", game);
+        while (end != '\n' && fscanf(f, " %u %6s ", &qty, color) == 2) {
+            size_t len = strlen(color) - 1;
+            end = color[len];
+
+            sum += (size_t)game;
+
+            switch(end) {
+                case ',': 
+                    color[len] = '\0';
+                    printf("%u %s, ", qty, color);
+                break;
+
+                case ';': 
+                    color[len] = '\0';
+                    printf("%u %s || ", qty, color);
+                break;
+                
+                default: 
+                    printf("%u %s\n", qty, color);
+                end='\n';
+            }
+        }
     }
     fclose(f);
+
+    printf("\n%zu\n", sum);
 }
 
 int main(void) {
     clock_t bench = clock();
     run();
-    printf("\nrunned in %f seconds.\n", (double)(clock() - bench)/CLOCKS_PER_SEC);
+    printf("\nrunned in %.3f seconds.\n", (double)(clock() - bench)/CLOCKS_PER_SEC);
 
     return 0;
 }
